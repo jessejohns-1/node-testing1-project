5f6c073c769f80edd507ef55ec86c241267094b1
Jesse <eoshout@Gmail.com>
2021-09-16 14:14:07 -0400

    test2

--- index.js
+++ index.js
@@ -23,7 +23,9 @@ function trimProperties(obj) {
  * trimPropertiesMutation({ name: '  jane  ' }) // returns the object mutated in place { name: 'jane' }
  */
 function trimPropertiesMutation(obj) {
-  // ✨ implement
+  for (let prop in obj){
+  obj[prop] = obj[prop].trim()
+  }return obj
 }
 
 /**
